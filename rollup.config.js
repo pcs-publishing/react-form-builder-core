ed29de57a716a6aeec12f409d3bf9589a6e8188a
Christopher Train <ctrain@pcs-publishing.com>
2022-05-09 10:48:22 +0100

    Configure build and publish 0.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,14 @@ import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 import external from 'rollup-plugin-peer-deps-external'
 import multi from '@rollup/plugin-multi-entry'
+import dts from 'rollup-plugin-dts'
+import del from 'rollup-plugin-delete'
 
 export default [
   {
     input: ['src/components/index.ts', 'src/hooks/index.ts', 'src/context/index.ts', 'src/utils/index.ts'],
     output: {
-      dir: 'dist',
+      file: 'dist/index.js',
       format: 'esm',
       sourcemap: true
     },
@@ -20,6 +22,14 @@ export default [
       typescript({ tsconfig: './tsconfig.json' }),
       terser(),
       multi()
+    ]
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [
+      dts(),
+      del({ targets: 'dist/types', hook: 'buildEnd' })
     ]
   }
 ]
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * as components from './components'
+export * as context from './context'
+export * as hooks from './hooks'
+export * as utils from './utils'
