30128feb68965c293c87c387492e9135ab010825
Christopher Train <ctrain@pcs-publishing.com>
2022-05-09 11:47:43 +0100

    Re-work exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,12 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 import external from 'rollup-plugin-peer-deps-external'
-import multi from '@rollup/plugin-multi-entry'
-import dts from 'rollup-plugin-dts'
-import del from 'rollup-plugin-delete'
 
 export default [
   {
-    input: ['src/components/index.ts', 'src/hooks/index.ts', 'src/context/index.ts', 'src/utils/index.ts'],
+    input: ['src/index.ts'],
     output: {
-      file: 'dist/index.js',
+      dir: 'dist',
       format: 'esm',
       sourcemap: true
     },
@@ -20,16 +17,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
-      terser(),
-      multi()
-    ]
-  },
-  {
-    input: 'dist/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [
-      dts(),
-      del({ targets: 'dist/types', hook: 'buildEnd' })
+      terser()
     ]
   }
 ]
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
-export * as components from './components'
-export * as context from './context'
-export * as hooks from './hooks'
-export * as utils from './utils'
+export {
+  ToolInstanceChildrenDragWrapper,
+  Draggable,
+  Form,
+  ToolInstanceRenderer
+} from './components'
+
+export {
+  FormContext,
+  ToolContext
+} from './context'
+
+export {
+  useFieldErrors,
+  useFormSubmit,
+  useFormValue,
+  useRegisterField
+} from './hooks'
+
+export {
+  formatItemsToToolInstances,
+  formatToolInstancesToItems,
+  generateLabelForFieldName,
+  getPathToRecord,
+  getToolInstanceByName,
+  getToolInstanceSiblings
+} from './utils'
